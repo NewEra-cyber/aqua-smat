dd03e7b5a8059bf1fe07bdd3cda321a11224d173
NewEra-cyber <marikus.m.o@gmail.com>
2026-02-02 18:34:34 +0300

    Add import for TemplateView in urls.py
    Import TemplateView for use in URL patterns.

--- aquarech/urls.py
+++ aquarech/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.urls import path, include
 
